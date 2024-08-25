d516c6caa6f82625dbc39e40a7ff68ab03b8737b
Jackson Dorsett <dorsettj@gmail.com>
2024-08-25 16:03:16 -0700

    added interface

---
+++ auth.interfaces/index.ts
@@ -0,0 +1,6 @@
+import IClientJWT from "./ClientJWT.interface";
+import IAuthenticatedRequest from "./AuthenticatedRequest.interface";
+import IBearerBody from "./IBearerBody";
+import IUserInfo from "./IUserInfo";
+
+export { IAuthenticatedRequest, IBearerBody, IClientJWT, IUserInfo };
